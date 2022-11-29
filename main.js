e41b6a4b30dbdafa2242ab6b33129e4b5f3469db
thevivekraut <vraut2306@gmail.com>
2022-11-29 11:56:30 +0530

    Expense Tracker Project

--- main.js
+++ main.js
@@ -1,91 +1,102 @@
-    const submitButton = document.getElementById("submit");
-    submitButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const cost = document.getElementById("cost").value;
-    const description = document.getElementById("movie").value;
-    const category = document.getElementById("selectList").value;
-    if (description.length > 0 && cost.length) {
-    const object = {
-    cost : cost,
-    description: description,   
-    category: category,
-    };
-    
- //posting objects to cru crud rest API
-     axios.post("https://crudcrud.com/api/b8f28564823d487cac624ee99a2f030f/expenseTracker",  object)
-    .then((response) => {
-       addNewLineElement(response.data);
-       console.log(response.data)
-     })
-     .catch((err)=> {
-       document.body.innerHTML = document.body.innerHTML ="<h4>Something went Wrong</h4>"; 
-       console.log(err)
-     })
- }
-});
 
-//getting array of object from crud crud
-axios.get("https://crudcrud.com/api/b8f28564823d487cac624ee99a2f030f/expenseTracker")
-.then((response) => {
-    console.log(response.data);
-    response.data.forEach((key) => {
-    console.log(key);
-    addNewLineElement(key);
-    for(let i=0;i<response.data.length;i++)
-    { 
-      const para = document.createElement("p");
-      para.setAttribute('id','uid');
-      para.textContent = response.data[i]._id;
-      document.body.appendChild(para).style.visibility="hidden";
-    }
-    });
-});
+//GETTING HTML ELEMENTS
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", (e) => {
+e.preventDefault();
+const cost = document.getElementById("cost").value;
+const description = document.getElementById("movie").value;
+const category = document.getElementById("selectList").value;
+if (description.length > 0 && cost.length) {
+const object = {
+cost : cost,
+description: description,   
+category: category,
+};
 
-
-  function addNewLineElement(object) {
-    const ul = document.getElementById("ExpenseItem");
-    const li = document.createElement("li");
-    li.appendChild(
-      document.createTextNode( object.cost + " " + object.description + " " + object.category + " ")
-    );
-
-    const vr = document.createElement("input");
-    vr.id = "ExpenseBtn";
-    vr.type = "button";
-    vr.value = "Edit Expense";
-    vr.addEventListener("click", () => {
-      document.getElementById("cost").value = object.cost;
-      document.getElementById("movie").value = object.description;
-      document.getElementById("selectList").value = object.category;
-
-
-      // Editing the objects of crud crud
-      axios.delete(`https://crudcrud.com/api/b8f28564823d487cac624ee99a2f030f/expenseTracker/${object._id}`)
-      .then((response) => {
-        console.log(response);
-      })  
-      .catch((err) => {
-        console.log(err);
-      })
-      li.remove();
-    });
-      li.appendChild(vr);
-  
-    const delBtn = document.createElement("input");
-    delBtn.type = "button";
-    delBtn.value = "Delete Expense";
-    delBtn.addEventListener("click", () => {
-
-      axios.delete(`https://crudcrud.com/api/b8f28564823d487cac624ee99a2f030f/${object._id}`)
-      .then((response) => {
-        console.log(response);
-      })  
-      .catch((err) => {
-        console.log(err);
-      })
-      li.remove();
-    });
-    li.appendChild(delBtn);
-    ul.appendChild(li);
+ //POSTING DATA TO CRUD CRUD REST API
+ async function postUser() {
+ try {
+  const exp = await axios.post("https://crudcrud.com/api/f86429d8166341458a9a6c7ffbfe1143/expenseTracker",  object)
+  console.log(exp.data);
+  addNewLineElement(exp.data);
+  }  
+  catch(error) {  
+    document.body.innerHTML = document.body.innerHTML ="<h4>Something went Wrong</h4>"; 
+    console.log('error');
   }
+};
+  postUser();
+}
+});
+
+// GETTING DATA FROM CRUD CRUD REST API
+async function fetchUser() {
+  const res = await fetch('https://crudcrud.com/api/f86429d8166341458a9a6c7ffbfe1143/expenseTracker');
+  const data = await res.json();
+  console.log(data);
+  data.forEach((key) => {
+        console.log(key);
+        addNewLineElement(key);
+  }
+ )};
+fetchUser();
+
+// SHOWING DATA ON WINDOW SCREEN
+  function addNewLineElement(object) {
+  const ul = document.getElementById("ExpenseItem");
+  const li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode( " Rs. " + object.cost + " " + object.description + " " + object.category + " ")
+  );
   
+  // CREATEING EDIT BUTTON 
+  const ebtn = document.createElement("input");
+  ebtn.id = "ExpenseEditBtn";
+  ebtn.type = "button";
+  ebtn.value = "Edit Expense";
+  li.appendChild(ebtn);
+
+  ebtn.addEventListener("click", () => {
+  document.getElementById("cost").value = object.cost;
+  document.getElementById("movie").value = object.description;
+  document.getElementById("selectList").value = object.category;
+
+    // EDITING USER 
+    async function EditUser(){
+    const edit = await axios.delete(`https://crudcrud.com/api/f86429d8166341458a9a6c7ffbfe1143/expenseTracker/${object._id}`)
+    try {
+      console.log(edit);
+    }  
+    catch(error){
+      console.log(error);
+    }
+    }
+    EditUser();
+    li.remove();
+    });
+  
+  // CREATING DELETE BUTTON
+  const delBtn = document.createElement("input");
+  delBtn.id = "ExpenseDeleteBtn"
+  delBtn.type = "button";
+  delBtn.value = "Delete Expense";
+  li.appendChild(delBtn);
+
+  // DELETE USER 
+  delBtn.addEventListener("click", () => {
+    async function deleteUser() {
+      try {
+        const del = await axios.delete(`https://crudcrud.com/api/f86429d8166341458a9a6c7ffbfe1143/expenseTracker/${object._id}`)
+        console.log(del);
+      }  
+      catch(error) {
+        console.log(error);
+      }
+    }
+  deleteUser();
+  li.remove();
+  });
+  
+  // ADDING UL ELEMENT 
+  ul.appendChild(li);
+}
